86425a56ad2a423aa299028ec786ce69d9052706
JormanOT <jormanortega34@gmail.com>
2024-01-18 20:02:33 -0400

    Modificado port env

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 import server from './server/_app.js'
 
-server.listen('5000', ()=> console.log('port listen to 5000'));
+server.listen(server.get('port'), ()=> console.log('port listen to 5000'));
--- server/_app.js
+++ server/_app.js
@@ -11,19 +11,20 @@ env.config();
 
 const app = express();
 
+app.set('port', process.env.PORT || '5000');
 app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
 app.use(FileUpload({
-    limits: { fileSize: 30 * 1024 * 1024 }
+  limits: { fileSize: 30 * 1024 * 1024 }
 }));
 app.use(routes);
 
 mongoose.connect(process.env.DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-    .then(() => console.log('DB Conected!'))
-    .catch((e) => console.error(e.message));
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('DB Conected!'))
+  .catch((e) => console.error(e.message));
 
 export default app;
